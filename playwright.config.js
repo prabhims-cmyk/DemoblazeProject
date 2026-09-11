452512b49362dd1eb3201f16c4bb5e5ef81c5e44
Prabhi <prabhims@gmail.com>
2026-09-11 16:08:41 +0530

    Add Allure reporter

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,10 @@ const config = ({
   expect:{
     timeout:40*1000
   },
-  reporter:'html',
+  reporter: [
+        ['html'],
+        ['allure-playwright']
+    ],
   use:{
     baseURL: 'https://www.demoblaze.com',
     browserName:'chromium',
